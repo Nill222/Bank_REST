776b437123e56c8c5b09b98dc590aea6b8194432
bogomolova <ibogomolllova@gmail.com>
2025-05-23 18:52:04 +0300

    обновлен User.java

--- src/main/java/com/example/bankcards/entity/User.java
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -3,9 +3,7 @@ package com.example.bankcards.entity;
 import com.example.bankcards.util.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.List;
@@ -16,8 +14,6 @@ import java.util.Set;
 @Table(name = "card_user")
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,6 +37,45 @@ public class User {
     @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Card> cards;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
 
     @Override
     public boolean equals(Object o) {
